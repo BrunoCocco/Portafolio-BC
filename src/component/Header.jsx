45af8a6bf09912802306d774bf29899bb20cd7b3
Bruno Cocco <brunoe.cocco@gmail.com>
2025-11-12 22:25:23 +0100

    todo conectado, bootstrap corriendo

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,7 +1,13 @@
+import './Component.css'
+
 function Header() {
   return (
-    <header>
-      <h2>Hola mundo desde el Header</h2>
+    <header className="container-fluid header-Fondo">
+     <div className='row'>
+         <h2 className='col-md-3'>Hoa mundo desde el Header</h2>
+           <h2 className='col-md-3 mt-4'>Hoa mundo desde el Header</h2>
+             <h2 className='col-md-3'>Hoa mundo desde el Header</h2>
+     </div>
     </header>
   )
 }
