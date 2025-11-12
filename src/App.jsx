bd66bc773f900a38338be11a36323a8c27a172fe
Bruno Cocco <brunoe.cocco@gmail.com>
2025-11-12 21:03:47 +0100

    armando arbol de trabajo

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import {} from 'react'
+import Header from './component/Header'
+import Portafolio from './component/Portafolio'
+import Footer from './component/Footer'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Header />
+
+    <Portafolio />
+    
+    <Footer />
+
+    </>
+  )
+}
+
+export default App
